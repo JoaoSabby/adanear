a333421d652ae4c46898edfe82e9a5dfeb898a5b
Joao B. G. de Brito <joao@sabby.com.br>
2026-04-23 06:22:31 -0300

    Add prep-focused regression tests for step_adanear

--- tests/testthat/test-step-adanear-validation.R
+++ tests/testthat/test-step-adanear-validation.R
@@ -17,3 +17,39 @@ test_that("adasyn feasibility error is readable", {
     "pelo menos"
   )
 })
+
+test_that("prep.step_adanear learns expected state for valid recipe", {
+  training <- data.frame(
+    y = factor(c("no", "no", "no", "yes")),
+    x1 = c(0.1, 0.2, 0.3, 0.4),
+    x2 = c(1, 2, 3, 4)
+  )
+
+  rec <- recipes::recipe(y ~ ., data = training) |>
+    step_adanear(y, increaseRatio = 0)
+
+  prepped <- recipes::prep(rec, training = training)
+  step <- prepped$steps[[1]]
+
+  expect_true(step$trained)
+  expect_identical(step$column, "y")
+  expect_setequal(step$predictors, c("x1", "x2"))
+  expect_identical(step$minorityLevel, "yes")
+  expect_identical(step$majorityLevel, "no")
+})
+
+test_that("prep.step_adanear blocks non numeric predictors", {
+  training <- data.frame(
+    y = factor(c("no", "no", "no", "yes")),
+    x1 = c(1, 2, 3, 4),
+    x_bad = c("a", "b", "c", "d")
+  )
+
+  rec <- recipes::recipe(y ~ ., data = training) |>
+    step_adanear(y)
+
+  expect_error(
+    recipes::prep(rec, training = training),
+    "precisam ser numericos"
+  )
+})
